88c9ffc1452c7651a51412006ec3687153ee3e9f
toho36 <toho0036@gmail.com>
2025-02-12 17:37:08 +0100

    added padding with tailwind styling

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -3,6 +3,8 @@ import { Content } from '@prismicio/client';
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
 import { PrismicNextLink } from '@prismicio/next';
 
+import { Bounded } from '@/components/Bounded';
+
 /**
  * Props for `Hero`.
  */
@@ -13,14 +15,14 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
       <PrismicRichText field={slice.primary.heading} />
       <PrismicRichText field={slice.primary.body} />
       <PrismicNextLink field={slice.primary.button} />
-    </section>
+    </Bounded>
   );
 };
 
