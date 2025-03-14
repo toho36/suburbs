764a4e21f60ef2ed44c41d74b932e56e9b596610
toho36 <toho0036@gmail.com>
2025-03-14 13:07:09 +0100

    Parallax + Slice Bundle
    added sticky positioning

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
 import { Metadata } from 'next';
 import { isFilled, asImageSrc } from '@prismicio/client';
-import { SliceZone } from '@prismicio/react';
+import { SliceComponentProps, SliceZone } from '@prismicio/react';
 
 import { createClient } from '@/prismicio';
 import { components } from '@/slices';
+import { Content } from '@prismicio/client';
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle('homepage');
+  const slices = bundleTextAndImageSlices(page.data.slices);
 
-  return <SliceZone slices={page.data.slices} components={components} />;
+  return (
+    <SliceZone
+      slices={slices}
+      components={{
+        ...components,
+        text_and_image_bundle: ({
+          slice,
+        }: SliceComponentProps<TextAndImageBundleSlice>) => (
+          <div>
+            <SliceZone slices={slice.slices} components={components} />
+          </div>
+        ),
+      }}
+    />
+  );
 }
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -31,4 +47,37 @@ export async function generateMetadata(): Promise<Metadata> {
         : undefined,
     },
   };
+}
+type TextAndImageBundleSlice = {
+  id: string;
+  slice_type: 'text_and_image_bundle';
+  slices: Content.TextAndImageSlice[];
+};
+
+function bundleTextAndImageSlices(
+  slices: Content.HomepageDocumentDataSlicesSlice[]
+) {
+  const res: (
+    | Content.HomepageDocumentDataSlicesSlice
+    | TextAndImageBundleSlice
+  )[] = [];
+
+  for (const slice of slices) {
+    if (slice.slice_type !== 'text_and_image') {
+      res.push(slice);
+      continue;
+    }
+
+    const bundle = res.at(-1);
+    if (bundle?.slice_type === 'text_and_image_bundle') {
+      bundle.slices.push(slice);
+    } else {
+      res.push({
+        id: `${slice.id}-bundle`,
+        slice_type: 'text_and_image_bundle',
+        slices: [slice],
+      });
+    }
+  }
+  return res;
 }
