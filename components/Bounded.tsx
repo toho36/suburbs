9f9a51fbc2942950798207a5a07c891ba27f2d80
vitek <toho0036@gmail.com>
2025-03-24 14:32:58 +0100

    vercel type error

--- components/Bounded.tsx
+++ components/Bounded.tsx
@@ -2,7 +2,7 @@ import { CSSProperties, ElementType, ReactNode } from 'react';
 import clsx from 'clsx';
 
 type BoundedProps = {
-  as?: any;
+  as?: ElementType;
   className?: string;
   style?: CSSProperties;
   children: ReactNode;
@@ -15,14 +15,14 @@ export function Bounded({
   ...restProps
 }: BoundedProps) {
   return (
-    <Comp
+    <div
       className={clsx(
-        'px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32',
+        'px-6 py-10 md:py-16 [.header+&]:pt-44 [.header+&]:md:pt-32',
         className
       )}
       {...restProps}
     >
-      <div className="mx-auto w-full max-w-6xl">{children}</div>
-    </Comp>
+      <div className="mx-auto max-w-6xl">{children}</div>
+    </div>
   );
 }
