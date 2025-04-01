3aeb9e9e4e298f54b7d98670771d687f3275f87a
toho36 <toho0036@gmail.com>
2025-04-01 18:56:42 +0200

    GSAP skateboard animations

--- slices/Hero/InteractiveSkateboard.tsx
+++ slices/Hero/InteractiveSkateboard.tsx
@@ -3,9 +3,10 @@
 import * as THREE from 'three';
 import { Skateboard } from '@/components/Skateboard';
 import { ContactShadows, Environment, Html } from '@react-three/drei';
-import { Canvas, useThree } from '@react-three/fiber';
-import { Suspense, useEffect, useRef } from 'react';
+import { Canvas, ThreeEvent, useThree } from '@react-three/fiber';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
+import { Hotspot } from './Hotspot';
 import { WavyPaths } from './WavyPaths';
 
 const INITIAL_CAMERA_POSITION = [1.5, 1, 1.4] as const;
@@ -51,6 +52,13 @@ function Scene({
   const containerRef = useRef<THREE.Group>(null);
   const originRef = useRef<THREE.Group>(null);
 
+  const [animating, setAnimating] = useState(false);
+  const [showHotspot, setShowHotspot] = useState({
+    front: true,
+    middle: true,
+    back: true,
+  });
+
   const { camera } = useThree();
 
   useEffect(() => {
@@ -91,6 +99,93 @@ function Scene({
     return () => window.removeEventListener('resize', setZoom);
   }, [camera]);
 
+  function onClick(event: ThreeEvent<MouseEvent>) {
+    event.stopPropagation();
+
+    const board = containerRef.current;
+    const origin = originRef.current;
+
+    if (!board || !origin || animating) return;
+
+    const { name } = event.object;
+
+    setShowHotspot((current) => ({ ...current, [name]: false }));
+
+    if (name === 'back') {
+      ollie(board);
+    } else if (name === 'middle') {
+      kickflip(board);
+    } else if (name === 'front') {
+      frontside360(board, origin);
+    }
+  }
+
+  function ollie(board: THREE.Group) {
+    jumpBoard(board);
+
+    gsap
+      .timeline()
+      .to(board.rotation, { x: -0.6, duration: 0.26, ease: 'none' })
+      .to(board.rotation, { x: 0.4, duration: 0.82, ease: 'power2.in' })
+      .to(board.rotation, { x: 0, duration: 0.12, ease: 'none' });
+  }
+
+  function kickflip(board: THREE.Group) {
+    jumpBoard(board);
+
+    gsap
+      .timeline()
+      .to(board.rotation, { x: -0.6, duration: 0.26, ease: 'none' })
+      .to(board.rotation, { x: 0.4, duration: 0.82, ease: 'power2.in' })
+      .to(
+        board.rotation,
+        {
+          z: `+=${Math.PI * 2}`,
+          duration: 0.78,
+          ease: 'none',
+        },
+        0.3
+      )
+      .to(board.rotation, { x: 0, duration: 0.12, ease: 'none' });
+  }
+
+  function frontside360(board: THREE.Group, origin: THREE.Group) {
+    jumpBoard(board);
+
+    gsap
+      .timeline()
+      .to(board.rotation, { x: -0.6, duration: 0.26, ease: 'none' })
+      .to(board.rotation, { x: 0.4, duration: 0.82, ease: 'power2.in' })
+      .to(
+        origin.rotation,
+        {
+          y: `+=${Math.PI * 2}`,
+          duration: 0.77,
+          ease: 'none',
+        },
+        0.3
+      )
+      .to(board.rotation, { x: 0, duration: 0.14, ease: 'none' });
+  }
+
+  function jumpBoard(board: THREE.Group) {
+    setAnimating(true);
+
+    gsap
+      .timeline({ onComplete: () => setAnimating(false) })
+      .to(board.position, {
+        y: 0.8,
+        duration: 0.51,
+        ease: 'power2.out',
+        delay: 0.26,
+      })
+      .to(board.position, {
+        y: 0,
+        duration: 0.43,
+        ease: 'power2.in',
+      });
+  }
+
   return (
     <group>
       <Environment files={'/hdr/warehouse-256.hdr'} />
@@ -107,17 +202,33 @@ function Scene({
               constantWheelSpin
             />
 
-            <mesh position={[0, 0.27, 0.9]} name="front">
+            <Hotspot
+              isVisible={!animating && showHotspot.front}
+              position={[0, 0.38, 1]}
+              color="#B8FC39"
+            />
+
+            <mesh position={[0, 0.27, 0.9]} name="front" onClick={onClick}>
               <boxGeometry args={[0.6, 0.2, 0.58]} />
               <meshStandardMaterial visible={false} />
             </mesh>
 
-            <mesh position={[0, 0.27, 0]} name="middle">
+            <Hotspot
+              isVisible={!animating && showHotspot.middle}
+              position={[0, 0.33, 0]}
+              color="#FF7A51"
+            />
+            <mesh position={[0, 0.27, 0]} name="middle" onClick={onClick}>
               <boxGeometry args={[0.6, 0.1, 1.2]} />
               <meshStandardMaterial visible={false} />
             </mesh>
 
-            <mesh position={[0, 0.27, -0.9]} name="back">
+            <Hotspot
+              isVisible={!animating && showHotspot.back}
+              position={[0, 0.35, -0.9]}
+              color="#46ACFA"
+            />
+            <mesh position={[0, 0.27, -0.9]} name="back" onClick={onClick}>
               <boxGeometry args={[0.6, 0.2, 0.58]} />
               <meshStandardMaterial visible={false} />
             </mesh>
